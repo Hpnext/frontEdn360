574ea30ede4f62e3688eaa68706a610206d70112
Hpnext <tui-lol@hotmail.com>
2026-08-12 11:46:36 -0300

    agora vai

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://projeto360-production.up.railway.app',
 });
 
 // Interceptor de Requisição (Envia o token)
