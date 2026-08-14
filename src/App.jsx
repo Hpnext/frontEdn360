09461b1814a1500e1bb5f7a1be5d4401ee672e90
Hpnext <tui-lol@hotmail.com>
2026-08-13 21:27:34 -0300

    Visualizar avaliação/id

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
 <Route 
   path="/visualizar-avaliacao/:id" 
   element={
-   <RotaProtegida papeisPermitidos={['ADMIN', 'GESTOR']}>
+   <RotaProtegida>
       <VisualizarAvaliacao />
     </RotaProtegida>
   } 
